90fbc2a29887d118f5366d3ed135b112941c987e
Louie Shin <unicus.fortis@gmail.com>
2024-03-16 21:41:04 +0900

    Initial Commit

---
+++ Bitrace/BitraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitraceApp.swift
+//  Bitrace
+//
+//  Created by LOUIE MAC on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BitraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitraceUITests/BitraceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BitraceUITestsLaunchTests.swift
+//  BitraceUITests
+//
+//  Created by LOUIE MAC on 3/16/24.
+//
+
+import XCTest
+
+final class BitraceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
