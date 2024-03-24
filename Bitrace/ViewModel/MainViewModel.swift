134c8cb82dc5be3ee51ef81816d97bbdca7a2035
Louie Shin <unicus.fortis@gmail.com>
2024-03-24 20:43:24 +0900

    [Update] MainViewModel - Bookmark 기능 대응 및 View 메서드 이관

--- Bitrace/ViewModel/MainViewModel.swift
+++ Bitrace/ViewModel/MainViewModel.swift
@@ -5,16 +5,24 @@
 //  Created by LOUIE MAC on 3/20/24.
 //
 
-import Foundation
+import SwiftUI
+import RealmSwift
 import Combine
 
 final class MainViewModel: ObservableObject {
     
     private let networkService = NetworkService.shared
+    private var task = RealmRepository.shared.fetchBookmarks()
     private var cancellable = Set<AnyCancellable>()
-
+    
     @Published var coinMarketArray:[CoinMarketModel] = []
-    @Published var bitcoinTicker: Ticker?
+    @Published var bitcoinTicker: TickerResponse?
+    @Published var storedMarketTickers:[TickerResponse] = []
+    
+    init() {
+        fetchCoinMarket()
+        RealmRepository.shared.checkRealmDirectory()
+    }
     
     func fetchCoinMarket() {
         print(#function, "네트워크 통신 시작함")
@@ -49,8 +57,69 @@ final class MainViewModel: ObservableObject {
             }
             
         } receiveValue: { [weak self] response in
-            self?.bitcoinTicker = response
+            self?.bitcoinTicker = response.first
+            print(response)
         }
         .store(in: &cancellable)
+    }
+    
+    func fetchStoredMarketTickers() {
+        var markets: [String] = []
+        task.forEach { markets.append($0.marketCode) }
+        let combined = markets.joined(separator: ",")
+        print("컴바인드", combined)
+        networkService.fetchRequest(endpoint: .ticker(market: combined),
+                                    decodeModel: Ticker.self )
+        .sink { result in
+            switch result {
+                
+            case .finished:
+                print(result)
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            
+        } receiveValue: { [weak self] response in
+            self?.storedMarketTickers = response
+        }
+        .store(in: &cancellable)
+    }
+    
+    func findMarketLink(market: String) -> CoinMarketModel? {
+        if let matched = coinMarketArray.first(where: { $0.market == market} ) {
+            return matched
+        } else {
+            return nil
+        }
+    }
+    
+    func makeTickerStatusIcon(change: String) -> Image {
+        switch change {
+        case "RISE":
+            return Image(.arrowUp)
+            
+        case "FALL":
+            return Image(.arrowDown)
+            
+        default: break
+        }
+        return Image(.horizontal)
+    }
+    
+    func makePriceColor(change: String) -> Color {
+        switch change {
+        case "RISE":
+            return .red
+            
+        case "FALL":
+            return .blue
+            
+        case "EVEN":
+            return .white
+            
+        default: break
+        }
+        return .white
     }
 }
