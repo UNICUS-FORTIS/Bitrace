48e3612c4d04a201e5ae340d8fac7c9f32c67d27
Louie Shin <unicus.fortis@gmail.com>
2024-03-24 20:40:51 +0900

    [Update] BannerView - UI 수정

--- Bitrace/View/MainPage/Components/BannerView.swift
+++ Bitrace/View/MainPage/Components/BannerView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 
-
 struct BannerView : View {
     
     @StateObject var viewModel: MainViewModel
@@ -24,21 +23,56 @@ struct BannerView : View {
                 }
                 .clipShape(RoundedRectangle(cornerRadius: 12))
                 .frame(maxWidth: .infinity)
-                .frame(height: 200)
+                .frame(height: 180)
             
             VStack(alignment: .leading) {
-                Spacer()
+                
                 Text("BitCoin")
-                    .font(.headline)
-                Text(String(viewModel.bitcoinTicker?.first?.tradePrice.formatted(.number) ?? 0.formatted(.number)))
                     .font(.title)
                     .bold()
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    Text("고가")
+                        .font(.caption)
+                    Text(String(viewModel.bitcoinTicker?.highPrice.formatted(.number) ??
+                                0.formatted(.number)))
+                    .font(.caption)
+                }
+                .foregroundStyle(.white)
+
+                HStack {
+                    Text("저가")
+                        .font(.caption)
+                    Text(String(viewModel.bitcoinTicker?.lowPrice.formatted(.number) ??
+                                0.formatted(.number)))
+                    .font(.caption)
+                }
+                .padding(.bottom, 6)
+                .foregroundStyle(.white)
+
+                Spacer()
+
+                HStack(spacing: 12) {
+                    Text(String(viewModel.bitcoinTicker?.tradePrice.formatted(.number) ?? 0.formatted(.number)))
+                        .font(.title)
+                        .bold()
+                        .foregroundStyle(viewModel.makePriceColor(change: viewModel.bitcoinTicker?.change ?? ""))
+
+
+                    viewModel.makeTickerStatusIcon(change: viewModel.bitcoinTicker?.change ?? "")
+                        .resizable()
+                        .frame(width: 16, height: 10)
+                        .foregroundStyle(viewModel.makePriceColor(change: viewModel.bitcoinTicker?.change ?? ""))
+                }
             }
-            .foregroundStyle(.white)
             .padding()
-            .padding(.vertical)
             .frame(maxWidth: .infinity, alignment: .leading)
         }
         .padding()
     }
+    
+    
+    
+
 }
